22a3e0c71b1efada4b0dd38d56dd0accacc876ae
Ajay Dev <ajaydevkmd@gmail.com>
2024-10-06 23:21:14 +0530

    express limiter for login api added

--- server/Routes/v1/userRoutes.js
+++ server/Routes/v1/userRoutes.js
@@ -1,9 +1,10 @@
-const { login, signup, logout } = require("../../Controllers/userControllers");
+const { login, signup, logout } = require("../../Controllers/userControllers")
+const limiter = require("../../Middlewares/apilimiter")
 
-const userRouter = require("express").Router();
+const userRouter = require("express").Router()
 
-userRouter.post("/signup", signup);
-userRouter.post("/login", login);
-userRouter.post("/logout", logout);
+userRouter.post("/signup", signup)
+userRouter.post("/login", limiter, login)
+userRouter.post("/logout", logout)
 
-module.exports = userRouter;
+module.exports = userRouter
